8e9e0e364db7bdf3c5f53562bc6e7e799ea4cfee
nondefyde <nondefyde@gmail.com>
2019-09-09 20:47:01 +0100

    fixed bug

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
-app.use('/contact', contactRouter);
+//app.use('/contact', contactRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
