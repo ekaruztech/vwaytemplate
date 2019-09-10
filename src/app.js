9f255c13dcf7636694f0f5470dd52ae2e858fd61
nondefyde <nondefyde@gmail.com>
2019-09-10 13:13:55 +0100

    modified landing page

--- src/app.js
+++ src/app.js
@@ -22,12 +22,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(sassMiddleware({
-    src: path.join(__dirname, 'public'),
-    dest: path.join(__dirname, 'public'),
-    indentedSyntax: true, // true = .sass and false = .scss
+    src: path.join(__dirname, '../public'),
+    dest: path.join(__dirname, '../public'),
+    indentedSyntax: true,
     sourceMap: true
 }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 
 app.use(routes);
 //app.use('/contact', contactRouter);
