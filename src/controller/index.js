ce89ec31e4a464a10b2382d34a01b3cf7f7cea3a
nondefyde <nondefyde@gmail.com>
2019-09-20 14:58:35 +0100

    update

--- src/controller/index.js
+++ src/controller/index.js
@@ -4,22 +4,28 @@ export const AppController = {
     index(req, res, next) {
         return res.render('index', { title: 'Divine Express' });
     },
-    entry(req, res, next) {
-        res.render('trips', { title: 'Divine Express', host: process.env.HOST });
-    },
     contact(req, res, next) {
         return res.render('contact', { title: 'Divine Express' });
     },
     about(req, res, next) {
         return res.render('about', { title: 'Divine Express' });
     },
-    async terminals(req, res, next) {
-        const page = req.query.page || 1;
-        const { _meta, data } = await APPRequest.getTerminals({ page });
-        let response = { title: 'Divine Express' };
-        if (data && _meta) {
-            response = { ...response, terminals: data, pagination: _meta.pagination }
-        }
-        res.render('terminals', response);
-    }
+	async entry(req, res, next) {
+		const { data: { social_auth_keys } } = await APPRequest.getAccount(process.env.VOOMSWAY_API_KEY);
+		console.log('social_auth_keys : ', social_auth_keys);
+		res.render('trips', {
+			title: 'Divine Express',
+			host: process.env.HOST,
+			social_auth_keys
+		});
+	},
+	async terminals(req, res, next) {
+		const page = req.query.page || 1;
+		const { _meta, data } = await APPRequest.getTerminals({ page });
+		if (data) {
+			res.render('terminals', { title: 'Captain Hamilton', terminals: data, pagination: _meta.pagination });
+		} else {
+			res.render('terminals', { title: 'Captain Hamilton', terminals: [], pagination: null });
+		}
+	}
 };
