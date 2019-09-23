7699da2ea89a1dcb5cb18bbef6d2d530dcefbd7b
nondefyde <nondefyde@gmail.com>
2019-09-23 12:28:41 +0100

    ui update

--- src/controller/request.js
+++ src/controller/request.js
@@ -4,43 +4,43 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const defaultOptions = {
-    baseURL: process.env.VOOMSWAY_API_HOST,
-    headers: {},
+	baseURL: process.env.VOOMSWAY_API_HOST,
+	headers: {},
 };
 
 const instance = axios.create(defaultOptions);
 
 instance.interceptors.request.use(
-    config => {
-        config.headers['x-api-key'] = process.env.VOOMSWAY_API_KEY;
-        return config;
-    },
-    error => {
-        // Do something with request error
-        return Promise.reject(error);
-    }
+	config => {
+		config.headers['x-api-key'] = process.env.VOOMSWAY_API_KEY;
+		return config;
+	},
+	error => {
+		// Do something with request error
+		return Promise.reject(error);
+	}
 );
 
 instance.interceptors.response.use(
-    response => {
-        // Do something with response data
-        return response.data;
-    },
-    error => {
-        if (error.response) {
-            // The request was made and the server responded with a status code
-            // that falls out of the range of 2xx
-            throw error.response.data;
-        } else if (error.request) {
-            // The request was made but no response was received
-            // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-            // http.ClientRequest in node.js
-            console.log('error request', error.request);
-        } else {
-            // Something happened in setting up the request that triggered an Error
-            console.log('Error', error.message);
-        }
-    }
+	response => {
+		// Do something with response data
+		return response.data;
+	},
+	error => {
+		if (error.response) {
+			// The request was made and the server responded with a status code
+			// that falls out of the range of 2xx
+			throw error.response.data;
+		} else if (error.request) {
+			// The request was made but no response was received
+			// `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+			// http.ClientRequest in node.js
+			console.log('error request', error.request);
+		} else {
+			// Something happened in setting up the request that triggered an Error
+			console.log('Error', error.message);
+		}
+	}
 );
 
 /**
@@ -51,36 +51,36 @@ instance.interceptors.response.use(
 const createRequest = config => instance(config);
 
 const APPRequest = {
-    /**
-     * Get Terminals of a company
-     * @param {Object} query a company
-     * @return {Object} res The response object
-     */
-    async getTerminals(query) {
-        const config = {
-            method: 'get',
-            url: '/terminals',
-            params: { population: JSON.stringify(['destinations']), ...query }
-        };
-        return createRequest(config)
-            .then(response => response,
-                err => {
-                    console.log('err >>>>>>>> ', err);
-                    return {};
-                });
-    },
-
-  async getAccount(apiKey) {
-    const config = {
-      method: 'get',
-	    url: `/api/account/${apiKey}`
-    };
-    return createRequest(config)
-      .then(response => response,
-        err => {
-          return {};
-        });
-  }
+	/**
+	 * Get Terminals of a company
+	 * @param {Object} query a company
+	 * @return {Object} res The response object
+	 */
+	async getTerminals(query) {
+		const config = {
+			method: 'get',
+			url: '/terminals',
+			params: { population: JSON.stringify(['destinations']), ...query }
+		};
+		return createRequest(config)
+			.then(response => response,
+				err => {
+					console.log('err >>>>>>>> ', err);
+					return {};
+				});
+	},
+	
+	async getAccount(apiKey) {
+		const config = {
+			method: 'get',
+			url: `/api/account/${apiKey}`
+		};
+		return createRequest(config)
+			.then(response => response,
+				err => {
+					return {};
+				});
+	}
 };
 
 export default APPRequest;
